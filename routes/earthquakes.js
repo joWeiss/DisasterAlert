80d56241910ac26462ee9144d1a2149a79571df7
joWeiss <jweissensel@mailbox.org>
2016-09-03 15:05:46 +0200

    Showing all earthquakes over 2.5 for the last day

--- routes/earthquakes.js
+++ routes/earthquakes.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request')
+
 module.exports = router;
 
 // middleware that is specific to this router
@@ -18,8 +20,26 @@ router.use('/', function (req, res, next) {
   next()
 })
 
-router.get('\*/bydist', function (req, res) {
+router.get('/all', function (req, res) {
+  let options = {
+      method: 'get',
+      url: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson'
+    }
+    request(options, function (err, response, body) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.status(200).send(body)
+      }
+    })
+})
+
+router.get('/bydist', function (req, res) {
+  let lon = req.query.lon
+  let lat = req.query.lat
   // list earthquakes nearby
+  console.log('Distance unknown.')
+  res.status(200).send({'number_ports': 0})
 })
 
 // TODO Error handling
@@ -27,7 +47,6 @@ router.use(function (err, req, res, next) {
   if (res.headersSent) {
     return next(err)
   }
-  res.status(500)
-  res.send('error', {error: err})
+  res.status(500).send(err)
 })
 
